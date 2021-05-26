da6c4acb1b8eefdf6b27fced6e4b69455348b26c
runoverPy <74095195+runoverPy@users.noreply.github.com>
2021-05-26 23:49:52 +0200

    simplifiation of code, removal of redundancies

--- soundboard.py
+++ soundboard.py
@@ -1,3 +1,4 @@
+from platform import win32_edition
 import time
 import keyboard
 import sounddevice as sd
@@ -9,15 +10,14 @@ import queue
 import sys
 from threading import Thread
 from playsound import playsound
+import json
 
-class Delayer(Thread):
-    def __init__(self):
-        Thread.__init__(self)
-    def run(self):
-        while True:
-            time.sleep(0.1)
-            if(keyboard.is_pressed("esc")):
-                break
+
+def create_filename(length):
+    result = ""
+    for i in range(length -4):
+        result += str(random.randint(0, 9))
+    return result + ".wav"
 
 
 def int_or_str(text):
@@ -27,79 +27,64 @@ def int_or_str(text):
     except ValueError:
         return text
 
-class Audio():
-    def __init__(self, filename, path, record_key):
-        self.filename = filename
-        self.fs = 44100  # Sample rate
-        self.record_key = record_key
-        self.path = path
 
-        
-    def play_audio(self):
-        playsound(self.filename)
-
-
-    def record_audio(self):
-        print("starting new recording")
-
-        self.parser = argparse.ArgumentParser(add_help=False)
-        self.parser.add_argument('-l', '--list-devices', action='store_true', help='show list of audio devices and exit')
-        self.args, remaining = self.parser.parse_known_args()
-        if self.args.list_devices:
-            print(sd.query_devices())
-            self.parser.exit(0)
-        self.parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter, parents=[self.parser])
-        self.parser.add_argument('filename', nargs='?', metavar='FILENAME', help='audio file to store recording to')
-        self.parser.add_argument('-d', '--device', type=int_or_str, help='input device (numeric ID or substring)')
-        self.parser.add_argument('-r', '--samplerate', type=int, help='sampling rate')
-        self.parser.add_argument('-c', '--channels', type=int, default=1, help='number of input channels')
-        self.parser.add_argument('-t', '--subtype', type=str, help='sound file subtype (e.g. "PCM_24")')
-        self.args = self.parser.parse_args(remaining)
-
-        self.q = queue.Queue()
-
-
-        def callback(indata, frames, time, status):
-            """This is called (from a separate thread) for each audio block."""
-            if status:
-                print(status, file=sys.stderr)
-            self.q.put(indata.copy())
-
-
-        if self.args.samplerate is None:
-            device_info = sd.query_devices(self.args.device, 'input')
-            # soundfile expects an int, sounddevice provides a float:
-            self.args.samplerate = int(device_info['default_samplerate'])
-        if self.args.filename is None:
-            self.args.filename = self.filename
-
-        # Make sure the file is opened before recording anything:
-        with sf.SoundFile(self.args.filename, mode='x', samplerate=self.args.samplerate,
-                            channels=self.args.channels, subtype=self.args.subtype) as file:
-            with sd.InputStream(samplerate=self.args.samplerate, device=self.args.device,
-                                channels=self.args.channels, callback=callback):
-                while True:
-                    file.write(self.q.get())
-                    if(not keyboard.is_pressed("`")):
-                        break             
-        
-        print("recording finished")
-
-
-
-class Button(Audio):
-    def __init__(self, filename, path, recording_key, trigger_key = ""):
+class Button:
+    def __init__(self, filename, recording_key, trigger_key = ""):
         self.trigger_key = trigger_key
         self.recording_key = recording_key
-        self.path = path
-        Audio.__init__(self, filename, path, self.recording_key)
+        self.filename = filename
         if self.trigger_key == "":
             self.record() 
         self.set_key()
 
 
+    def identity(self):
+        return {"file": self.filename, "key": self.trigger_key}
+
+
     def record(self):
-        self.record_audio()
+        print("starting new recording")
+
+        parser = argparse.ArgumentParser(add_help=False)
+        parser.add_argument('-l', '--list-devices', action='store_true', help='show list of audio devices and exit')
+        args, remaining = parser.parse_known_args()
+        if args.list_devices:
+            print(sd.query_devices())
+            parser.exit(0)
+            sys.exit("Error: no input devices found")
+        parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter, parents=[parser])
+        parser.add_argument('filename', nargs='?', metavar='FILENAME', help='audio file to store recording to')
+        parser.add_argument('-d', '--device', type=int_or_str, help='input device (numeric ID or substring)')
+        parser.add_argument('-r', '--samplerate', type=int, help='sampling rate')
+        parser.add_argument('-c', '--channels', type=int, default=1, help='number of input channels')
+        parser.add_argument('-t', '--subtype', type=str, help='sound file subtype (e.g. "PCM_24")')
+        args = parser.parse_args(remaining)
+
+        savingqueue = queue.Queue()
+
+
+        if args.samplerate is None:
+            device_info = sd.query_devices(args.device, 'input')
+            args.samplerate = int(device_info['default_samplerate'])
+        if args.filename is None:
+            print("filename is none")
+            args.filename = self.filename
+
+        # Make sure the file is opened before recording anything:
+        with sf.SoundFile(args.filename, mode='x', samplerate=args.samplerate, channels=args.channels, subtype=args.subtype) as file:
+
+            def callback(indata, frames, time, status):
+                """This is called (from a separate thread) for each audio block."""
+                # nonlocal savingqueue
+                if status:
+                    print(status, file=sys.stderr)
+                savingqueue.put(indata.copy())
+
+            with sd.InputStream(samplerate=args.samplerate, device=args.device, channels=args.channels, callback=callback):
+                while keyboard.is_pressed("`"):
+                    file.write(savingqueue.get())
+         
+        print("recording finished")
         time.sleep(0.5)
         print("awaiting button designation")
         
@@ -113,70 +98,56 @@ class Button(Audio):
                 time.sleep(0.05)
             return re.match(r"(\S+\()(\S)((\s|\S)+)", str(return_value)).group(2)
         self.trigger_key = return_key()
-        print("key input (", self.trigger_key, ") recieved. compiling button")
-        self.file = open("soundboard/sound_masterfile.txt", "a")
-        self.file.write(str(self.filename + " " + self.trigger_key + "\n"))
-        self.file.close()
+        print(f"key input ({self.trigger_key}) recieved.")
 
     def set_key(self):
         def callback():
-            self.play_audio()
+            playsound(self.filename)
         keyboard.add_hotkey(self.trigger_key, callback)
-
-
-def create_filename(length):
-    result = ""
-    for i in range(length -4):
-        result += str(random.randint(0, 9))
-    return result + ".wav"
-
 
 
 class Soundboard():
     def __init__(self):
         self.recording_key = "`"
-        self.path = ""
         self.used_filenames = []
         self.filename_length = 9
         self.all_buttons = []
-        self.button_nums = 0
         print("restoring previous buttons")
         self.restore_buttons()
 
+    def run(self):
+        self.restore_buttons()
 
     def restore_buttons(self):
-        try:
-            self.savefile = open("soundboard/sound_masterfile.txt", "r+")
-            for self.line in self.savefile:
-                try:
-                    self.recalled_filename = re.match(r"(\S+\.wav)(\s+)(\S+)", self.line) 
-                    self.used_filenames.append(self.recalled_filename.group(1))
-                    self.all_buttons.append(Button(self.recalled_filename.group(1), self.path, self.recording_key, self.recalled_filename.group(3)))
-                    self.button_nums += 1
-                except AttributeError:
-                    print("error, contents of line improper:", self.line)
-        except FileNotFoundError:
-            print("FileNotFoundError called, new file created")
-            self.newfile = open("soundboard/sound_masterfile.txt", "w")
-            self.newfile.close()
-        print(len(self.all_buttons), " buttons were restored")
+        with open("soundboard/keyfile.json", "r") as keys:
+            for soundbyte in json.load(keys):
+                self.all_buttons.append(Button(soundbyte["file"], self.recording_key, soundbyte["key"]))
+                self.used_filenames.append(soundbyte["file"])
+        print(f"{len(self.all_buttons)} buttons were restored")
+
+
+    def save_buttons(self):
+        with open("soundboard/keyfile.json", "w") as keys:
+            json.dump(list(button.identity() for button in self.all_buttons), keys, indent=4)
+
 
     def create_filename(self, length):
-        self.result = ""
-        for self.i in range(length -4):
-            self.result += str(random.randint(0, 9))
-        return self.result + ".wav"
+        result = ""
+        for i in range(length):
+            result += str(random.randint(0, 9))
+        return result + ".wav"
+
 
     def create_button(self):
         keyboard.wait("`")    
         print("button creation in progress")
-        self.all_buttons.append(Button(create_filename(9), "`", self.recording_key))
-        self.button_nums += 1
+        self.all_buttons.append(Button(create_filename(9), self.recording_key))
 
-
-soundboard = Soundboard()
-while True:
-    if keyboard.is_pressed("esc"):
-        break
-    elif keyboard.is_pressed("`"):
-        soundboard.create_button()
+if __name__ == "__main__":
+    soundboard = Soundboard()
+    while True:
+        if keyboard.is_pressed("esc"):
+            break
+        elif keyboard.is_pressed("`"):
+            soundboard.create_button()
+    soundboard.save_buttons()
